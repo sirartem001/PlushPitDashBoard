2b601849229f37307ac0bea148604dcb29300035
sirartem001 <33823479+sirartem001@users.noreply.github.com>
2024-11-12 20:26:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,15 @@ import pandas as pd
 import streamlit as st
 import json
 import io
-from datetime import datetime
+import datetime
 import requests
+from metrics import *
 
 API_URL = 'https://api-seller.ozon.ru'
 API_KEY = '22c5ec5b-5e3f-4002-afca-95fbbdae08aa'
 CLIENT_ID = '1997569'
 ERROR = ""
-now = datetime.now()
+now = datetime.datetime.now()
 headers = {
     'Client-Id': CLIENT_ID,
     'Api-Key': API_KEY,
@@ -21,11 +22,8 @@ st.set_page_config(layout="wide", page_title="PlushPit DashBoard")
 st.title("PlushPit DashBoard")
 
 
-
-
-
 def get_ozon_prod_state(status):
-    now = datetime.now()
+    now = datetime.datetime.now()
     date1 = str(now.year - 1) + '-' + str(now.month).zfill(2) + '-' + str(now.day).zfill(2) + 'T' + str(now.hour).zfill(
         2) + ':' + str(now.minute).zfill(2) + ':' + str(now.second).zfill(2) + '.000Z'
     date2 = str(now.year) + '-' + str(now.month).zfill(2) + '-' + str(now.day).zfill(2) + 'T' + str(now.hour).zfill(
@@ -57,6 +55,7 @@ def get_ozon_prod_state(status):
     df = pd.DataFrame(df[0].values.tolist())
     return df
 
+
 def get_ozon_warehouse_state(last_id):
     body = {
         "dir": "DESC",
@@ -75,7 +74,7 @@ def get_ozon_warehouse_state(last_id):
         json.dump(jason['result'], json_file, indent=4)
     df = pd.read_json('tmp.json')
     if df.empty:
-      return pd.DataFrame(), ''
+        return pd.DataFrame(), ''
     last = df.iloc[0]['last_id']
     df = df['items'].apply(pd.Series)
     df = pd.concat([df, df['stocks'].apply(pd.Series)], axis=1)
@@ -85,13 +84,14 @@ def get_ozon_warehouse_state(last_id):
     df = resolve_x2(df[['offer_id', 'present', 'reserved']], ['present', 'reserved'])
     return df, last
 
+
 def get_fbo():
     df, last_id = get_ozon_warehouse_state("")
     while True:
-      newdf, last_id = get_ozon_warehouse_state(last_id)
-      if newdf.empty:
-        return df
-      df = pd.concat(df, newdf)
+        newdf, last_id = get_ozon_warehouse_state(last_id)
+        if newdf.empty:
+            return df
+        df = pd.concat(df, newdf)
 
 
 def resolve_x2(df, column_names):
@@ -101,11 +101,11 @@ def resolve_x2(df, column_names):
         if string.startswith('x2_'):
             it = df.index[df['offer_id'] == string[3:]].tolist()
             for column_name in column_names:
-              quantity = df.loc[df['offer_id'] == string][column_name].tolist()[0]
-              if len(it) == 0:
-                  df.loc[len(df.index)] = [string[3:], quantity]
-              else:
-                  df.at[it[0], column_name] = quantity * 2 + df.at[it[0], column_name]
+                quantity = df.loc[df['offer_id'] == string][column_name].tolist()[0]
+                if len(it) == 0:
+                    df.loc[len(df.index)] = [string[3:], quantity]
+                else:
+                    df.at[it[0], column_name] = quantity * 2 + df.at[it[0], column_name]
             x2_names.append(string)
     return df.loc[~df['offer_id'].isin(x2_names)]
 
@@ -113,12 +113,13 @@ def resolve_x2(df, column_names):
 def get_in_way():
     in_way = get_ozon_prod_state("delivering")
     in_way = pd.concat([in_way, get_ozon_prod_state("awaiting_deliver")])
-    #in_way = pd.concat([in_way, get_ozon_prod_state("awaiting_packaging")])
-    #in_way = pd.concat([in_way, get_ozon_prod_state("cancelled")])
+    # in_way = pd.concat([in_way, get_ozon_prod_state("awaiting_packaging")])
+    # in_way = pd.concat([in_way, get_ozon_prod_state("cancelled")])
     in_way = in_way.groupby('offer_id')['quantity'].sum().reset_index()
     in_way = resolve_x2(in_way, ['quantity'])
     in_way.columns = ['offer_id', 'in_way']
     return in_way.fillna(0)
+
 
 def get_pivot():
     response = requests.get(
@@ -131,6 +132,7 @@ def get_pivot():
     df.columns = ['offer_id', 'pivot']
     df = df.fillna(0)
     return df
+
 
 def get_transfer_data(order_id):
     body = {
@@ -147,7 +149,6 @@ def get_transfer_data(order_id):
         json.dump(jason['items'], json_file, indent=4)
     df = pd.read_json('tmp.json')
     return df.drop(columns=['icon_path', 'sku', 'name'])
-    
 
 
 def get_ozon_transfer():
@@ -162,9 +163,9 @@ def get_ozon_transfer():
             ]
         },
         "paging": {
-          "from_supply_order_id": 0,
-          "limit": 100
-          }
+            "from_supply_order_id": 0,
+            "limit": 100
+        }
     }
     response = requests.post(API_URL + "/v2/supply-order/list", json=body, headers=headers)
     jason = response.json()
@@ -176,17 +177,19 @@ def get_ozon_transfer():
     id = pd.read_json('tmp.json').values.tolist()
     order_id = []
     for i in id:
-      order_id += i
-    df = pd.DataFrame({'offer_id':[], 'quantity':[]})
+        order_id += i
+    df = pd.DataFrame({'offer_id': [], 'quantity': []})
     for i in order_id:
-      df = pd.concat([df, get_transfer_data(i)])
+        df = pd.concat([df, get_transfer_data(i)])
     df = df.groupby('offer_id')['quantity'].sum().reset_index()
     df = resolve_x2(df, ['quantity'])
     df.columns = ['offer_id', 'transfer']
     return df
 
+
 def foo(x, y, z, w, e):
     return x - y - z - w - e
+
 
 @st.cache_data
 def load_dataWareHouse():
@@ -204,9 +207,10 @@ def load_dataWareHouse():
     result['warehouse'] = result.apply(lambda x: foo(x.pivot, x.reserved, x.in_way, x.present, x.transfer), axis=1)
     return result.reset_index(), TIME
 
+
 def refreshWareHouse():
     load_dataWareHouse.clear()
-    df, TIMEWareHouse = load_dataWareHouse()
+    dfWareHouse, TIMEWareHouse = load_dataWareHouse()
 
 
 def resolve_actions(actions):
@@ -216,6 +220,7 @@ def resolve_actions(actions):
     for action in actions:
         titles += action['title'] + '; '
     return titles
+
 
 def get_all_coast():
     body = {
@@ -229,36 +234,41 @@ def get_all_coast():
     jason = response.json()
     assert 'result' in jason, str(jason['message'])
     return jason['result']
-    
-@st.cache_data  
+
+
+@st.cache_data
 def load_dataPrice():
     TIME = str(now.year) + '-' + str(now.month).zfill(2) + '-' + str(now.day).zfill(2) + ' ' + str(now.hour + 3).zfill(
         2) + ':' + str(now.minute).zfill(2) + ':' + str(now.second).zfill(2)
     df1 = pd.json_normalize(get_all_coast()['items'])
-    
+
     df1 = df1[['offer_id', 'price.price', 'price.marketing_price', 'commissions.sales_percent',
-             'commissions.fbo_deliv_to_customer_amount', 'marketing_actions.actions']]
-    
-    df1.columns = ['offer_id', ',базовая цена', 'цена на карточке', 'комиссия за продажу', 'комиссия за доставку покупателю',
-                  'акции']
+               'commissions.fbo_deliv_to_customer_amount', 'marketing_actions.actions']]
+
+    df1.columns = ['offer_id', ',базовая цена', 'цена на карточке', 'комиссия за продажу',
+                   'комиссия за доставку покупателю',
+                   'акции']
     df1['акции'] = df1['акции'].apply(lambda d: d if isinstance(d, list) else [])
     df1['акции'] = df1['акции'].apply(resolve_actions)
-    df1['комиссия за продажу'] = pd.to_numeric(pd.to_numeric(df1['комиссия за продажу']) * pd.to_numeric(df1['цена на карточке'])).div(100.)
+    df1['комиссия за продажу'] = pd.to_numeric(
+        pd.to_numeric(df1['комиссия за продажу']) * pd.to_numeric(df1['цена на карточке'])).div(100.)
     return df1, TIME
+
 
 def refreshPrice():
     load_dataPrice.clear()
     Price, TIMEPrice = load_dataWareHouse()
 
-tab_titles = ['Остатки на складе', 'Цены']
-tab1, tab2 = st.tabs(tab_titles)
+
+tab_titles = ['Остатки на складе', 'Цены', 'Графики метрик']
+tab1, tab2, tab3 = st.tabs(tab_titles)
 with tab1:
-    df, TIMEWareHouse = load_dataWareHouse()
-    
-    st.text("Последнее обновление: " +TIMEWareHouse)
+    dfWareHouse, TIMEWareHouse = load_dataWareHouse()
+
+    st.text("Последнее обновление: " + TIMEWareHouse)
     st.button("↻ refresh", on_click=refreshWareHouse, key=0)
     st.dataframe(
-        df.sort_values("offer_id"),
+        dfWareHouse.sort_values("offer_id"),
         use_container_width=True
     )
     st.text(ERROR)
@@ -270,8 +280,28 @@ with tab1:
     st.text("warehouse - лежат на нашем складе")
 with tab2:
     Price, TIMEPrice = load_dataPrice()
-    st.text("Последнее обновление: " +TIMEPrice)
+    st.text("Последнее обновление: " + TIMEPrice)
     st.button("↻ refresh", on_click=refreshPrice, key=1)
     st.table(
         Price.sort_values("offer_id")
     )
+options = []
+articl = []
+def plot():
+    article = []
+    for i in range(len(options)):
+        if options[i]:
+            article.append(offer_ids[i])
+    with tab3:
+        print_graph_for_offer_id(article)
+
+
+with tab3:
+    st.write('Выбираем нужные артикулы:')
+    columns = st.columns(5)
+    offer_ids = get_offer_id_list()
+    col = 0
+    for offer_id in offer_ids:
+        options.append(columns[col % 5].checkbox(offer_id))
+        col += 1
+    st.button("Отрисовать графики", on_click=plot, key=2)
