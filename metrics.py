36db9f511884d55ddab502f0ba231963ff1f123c
sirartem001 <33823479+sirartem001@users.noreply.github.com>
2024-11-12 20:27:19 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,3 +1,5 @@
+import datetime
+
 import streamlit as st
 import pandas as pd
 import requests as r
@@ -6,14 +8,13 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 from time import sleep
 
-
 API_KEY = '22c5ec5b-5e3f-4002-afca-95fbbdae08aa'
 CLIENT_ID = '1997569'
 
 
 # список артикулов
-def articl_list(client_id, api_key, data):
-    headers = {'Content-type': 'application/json', 'Client-Id': client_id, 'Api-Key': api_key}
+def articl_list(data):
+    headers = {'Content-type': 'application/json', 'Client-Id': CLIENT_ID, 'Api-Key': API_KEY}
     url = 'https://api-seller.ozon.ru/v2/product/list'
     data = data
     response = r.post(url, data=json.dumps(data), headers=headers)
@@ -28,8 +29,8 @@ def articl_list(client_id, api_key, data):
 
 
 # отчет о заказах
-def create_orders_report(client_id, key, data):
-    headers = {'Content-type': 'application/json', 'Client-Id': client_id, 'Api-Key': key}
+def create_orders_report(data):
+    headers = {'Content-type': 'application/json', 'Client-Id': CLIENT_ID, 'Api-Key': API_KEY}
     url = 'https://api-seller.ozon.ru/v1/report/postings/create'
     data = data
     response = r.post(url, data=json.dumps(data), headers=headers, verify=True)
@@ -41,26 +42,29 @@ def create_orders_report(client_id, key, data):
 
 
 # получение ссылки на отчет
-def get_report_link(client_id, key, report):
-    headers = {'Content-type': 'application/json', 'Client-Id': client_id, 'Api-Key': key}
+def get_report_link(report):
+    headers = {'Content-type': 'application/json', 'Client-Id': CLIENT_ID, 'Api-Key': API_KEY}
     url = 'https://api-seller.ozon.ru/v1/report/info'
     data = {
         "code": report
     }
-
-    response = r.post(url, data=json.dumps(data), headers=headers, verify=True)
-    if response.status_code == 200:
-        answer = eval(response.content.decode('utf-8'))
-        if answer.get('result').get('status') == 'success':
-            return response.status_code, answer.get('result').get('file')
+    while True:
+        response = r.post(url, data=json.dumps(data), headers=headers, verify=True)
+        if response.status_code == 200:
+            answer = eval(response.content.decode('utf-8'))
+            if answer.get('result').get('status') == 'success':
+                return response.status_code, answer.get('result').get('file')
+            if answer.get('result').get('status') == 'processing' or answer.get('result').get('status') == 'waiting':
+                sleep(2)
+                continue
+            else:
+                return response.status_code, "something_wrong"
         else:
-            return response.status_code, answer.get('result').get('status')
-    else:
-        return response.status_code, "something_wrong"
+            return response.status_code, "something_wrong"
 
 
 def get_offer_id_list():
-    x = articl_list(client_id=CLIENT_ID, api_key=API_KEY, data={
+    x = articl_list(data={
         "filter": {},
         "last_id": "",
         "limit": 1000
@@ -68,14 +72,20 @@ def get_offer_id_list():
     return [i['offer_id'] for i in x[1]]
 
 
-def print_graph_for_offer_id(offer_ids, date_from, date_to):
-    fig = make_subplots(rows=len(offer_ids), cols=1, shared_yaxes=True)
+def get_date(date):
+    return date[:10]
+
+
+def print_graph_for_offer_id(offer_ids):
+    date_to = datetime.date.today()
+    delta = datetime.timedelta(30)
+    date_from = date_to - delta
     traces = []
     for offer_id in offer_ids:
-        ass = create_orders_report(CLIENT_ID, API_KEY, data={
+        ass = create_orders_report(data={
             "filter": {
-                "processed_at_from": "2024-10-28T03:00:00.861Z",
-                "processed_at_to": "2024-11-12T23:59:59.861Z",  # перевод в UTC+3
+                "processed_at_from": date_from.strftime('%Y-%m-%d') + "T00:00:00.861Z",
+                "processed_at_to": date_to.strftime('%Y-%m-%d') + "T00:00:00.861Z",
                 "delivery_schema": [
                     "fbo"
                 ],
@@ -89,24 +99,21 @@ def print_graph_for_offer_id(offer_ids, date_from, date_to):
             "language": "EN"
         })
         sleep(1)
-        link = get_report_link(CLIENT_ID, API_KEY, ass[1])
+        link = get_report_link(ass[1])
         report = pd.read_csv(link[1], sep=';')
         orders = report[['Article code', 'Quantity', 'Accepted for processing', 'Status']]
         orders = orders.rename(
             columns={'Article code': 'article', 'Quantity': 'quantity', 'Accepted for processing': 'date',
                      'Status': 'status'})
-        for i in range(orders.index.min(), orders.index.max() + 1):
-            orders.date[i] = orders.date[i][:10]
+        orders['date'] = orders['date'].apply(get_date)
         fig = px.bar(orders, x="date", y="quantity", color="status", title=offer_id)
-        traces.append([])
+        traces.append([[], offer_id])
         for trace in range(len(fig['data'])):
-            traces[-1].append(fig["data"][trace])
-    this_figure = make_subplots(rows=len(offer_ids), cols=1)
+            traces[-1][0].append(fig["data"][trace])
+    this_figure = make_subplots(rows=len(offer_ids), cols=1, subplot_titles=[traces[i][1] for i in range(len(traces))])
     for i in range(len(traces)):
-        for trace in traces[i]:
+        for trace in traces[i][0]:
             this_figure.add_trace(trace, row=i + 1, col=1)
-    st.plotly_chart(this_figure)
-
-
-offer_ids = ['tochilka_bear_pink_dark', 'tochilka_cat_yellow']
-print_graph_for_offer_id(offer_ids, 0, 0)
+    height = 400 * len(traces)
+    this_figure.update_layout(height=height)
+    st.plotly_chart(this_figure, use_container_width=True, height=height)
